710b2b1c4a8ace2d5fc50e1182463261cf5f6d51
dominic <dom.orchard@gmail.com>
2016-03-17 18:30:34 +0000

    as part of the fix for #9, language-fortran-0.5.1 is needed

--- tests/TestSuite.hs
+++
@@ -1,8 +0,0 @@
-import Test.HUnit
-
-import TransformationTests.Common
-
-main :: IO ()
-main = do runTestTT (TestList [test toArgsIntegration, test toArgsIntegration2, test (assertEqual "FALSE" 0 1)])
-          return ()
-       
