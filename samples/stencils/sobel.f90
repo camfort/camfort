8bc25a898cec842ebd12b2e4dd75c9595f6884e3
dominic <dom.orchard@gmail.com>
2016-07-03 20:29:14 +0100

    Sobel convolution example

---
+++ samples/stencils/sobel.f90
@@ -0,0 +1,22 @@
+program sten
+  
+real (kind=8), dimension(0:imax,1:jmax) :: a
+real (kind=8), dimension(0:imax,1:jmax) :: b
+real gx, gy
+
+do j=1, jmax-1
+   do i=1, imax-1
+
+      gx = -a(i-1,j-1) + a(i+1,j-1)  &
+      &  + -2*a(i-1,j) + 2*a(i+1, j) &
+      & - a(i-1,j+1) + a(i+1,j+1)
+
+      gy = -a(i-1,j-1) - 2*a(i,j-1) &
+        & - a(i+1,j-1)              &
+        & + a(i-1,j+1) + 2*a(i,j+1) + a(i+1,j+1)
+
+    b(i,j) = sqrt(gx*gx+gy*gy)
+   enddo
+enddo
+
+end program
