6733253f81661e2e56779a82574b943fba345c62
dominic <dom.orchard@gmail.com>
2016-07-11 10:08:09 +0100

    unit test stubs

---
+++ tests/Camfort/Specification/Units/ex1.f90
@@ -0,0 +1,29 @@
+program energy
+ != unit :: speed = m/s
+
+ != unit (m / s** (2)) :: gravity
+ != unit (m) :: height
+ != unit (kg) :: mass
+  real, parameter :: mass = 3.00, gravity = 9.81, height = 4.20
+ != unit (kg m** (2) / s** (2)) :: potential_energy
+  real :: potential_energy
+
+ != unit (1) :: half
+ != unit (speed) :: velocity
+  real, parameter :: half = 0.5, velocity = 4.00
+ != unit (kg m** (2) / s** (2)) :: kinetic_energy
+ != unit (kg m** (2) / s** (2)) :: total_energy
+  real :: kinetic_energy, total_energy
+
+  potential_energy = mass * gravity * height
+  kinetic_energy = half * mass * square(velocity)
+
+  total_energy = potential_energy + kinetic_energy
+
+  contains
+
+  real function square(x)
+    real x
+    square = x * x
+  end function square
+end program energy
--- tests/Camfort/Specification/UnitsSpec.hs
+++ tests/Camfort/Specification/UnitsSpec.hs
@@ -1,9 +1,21 @@
 module Camfort.Specification.UnitsSpec (spec) where
 
+import Camfort.Input
+import Camfort.Functionality
+
 import Test.Hspec
 import Test.QuickCheck
 import Test.Hspec.QuickCheck
 
 spec :: Spec
 spec = describe "Unit specifications" $ do
-         describe "Stub" $ do return ()
+         describe "Integration tests" integration
+
+
+integration = do
+   let file1 = "tests/Camfort/Specification/Units/ex1.f90"
+   program1 <- runIO $ readForparseSrcDir file1 []
+
+   let file2 = "tests/Camfort/Specification/Units/ex2.f90"
+   program2 <- runIO $ readForparseSrcDir file2 []
+   return ()
