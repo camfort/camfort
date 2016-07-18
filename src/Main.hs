39e96991f3ce12452233d6339dbc251eb16d8cc8
dominic <dom.orchard@gmail.com>
2016-07-18 12:10:23 +0100

    version number to 0.801

--- src/Main.hs
+++ src/Main.hs
@@ -129,7 +129,7 @@ analyses =
      ("units-synth", (unitsSynth, "unit-of-measure synthesise specs.")) ]
 
 -- * Usage and about information
-version = "0.800"
+version = "0.801"
 introMsg = "CamFort " ++ version ++ " - Cambridge Fortran Infrastructure."
 usage = "Usage: camfort <MODE> <INPUT> [OUTPUT] [OPTIONS...]\n"
 menu =
