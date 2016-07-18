82cec595f7ef0c52dd7368f619ec992d076ce070
Matthew Danish <mrd45@cam.ac.uk>
2016-07-18 16:08:00 +0100

    some tests for backend

--- tests/Camfort/Specification/UnitsSpec.hs
+++ tests/Camfort/Specification/UnitsSpec.hs
@@ -8,6 +8,7 @@ import Camfort.Input
 import Camfort.Functionality
 import Camfort.Output
 import Camfort.Specification.Units
+import Camfort.Specification.Units.InferenceBackend
 import Camfort.Specification.Units.Environment
 
 import Test.Hspec
@@ -15,8 +16,26 @@ import Test.QuickCheck
 import Test.Hspec.QuickCheck
 
 spec :: Spec
-spec = describe "Unit specifications" $ do
-         describe "Integration tests of infer and synthesise" integration
+spec = do
+  describe "Unit Inference Backend" $ do
+    describe "Flatten constraints" $ do
+      it "testCons1" $ do
+        flattenConstraints testCons1 `shouldBe` testCons1_flattened
+    describe "Shift terms" $ do
+      it "testCons1" $ do
+        map shiftTerms (flattenConstraints testCons1) `shouldBe` testCons1_shifted
+      it "testCons2" $ do
+        map shiftTerms (flattenConstraints testCons2) `shouldBe` testCons2_shifted
+    describe "Consistency" $ do
+      it "testCons1" $ do
+        let (_, inconsists, _) = constraintsToMatrix testCons1
+        inconsists `shouldNotSatisfy` null
+      it "testCons2" $ do
+        let (_, inconsists, _) = constraintsToMatrix testCons2
+        inconsists `shouldSatisfy` null
+
+-- describe "Unit specifications" $ do
+--   describe "Integration tests of infer and synthesise" integration
 
 
 integration = do
@@ -50,3 +69,49 @@ doIntegration c fname expInfer = do
    it ("(" ++ show c ++ ") - " ++ fname ++ " comapare expected out") $
        (B.unpack $ mkOutputText ("exp." ++ fname) (head $ mkOutputFileForpar ps [ps']))
           `shouldBe` expect
+
+--------------------------------------------------
+
+testCons1 = [ UnitEq (UnitName "kg") (UnitName "m")
+            , UnitEq (Determined "x") (UnitName "m")
+            , UnitEq (Determined "y") (UnitName "kg")]
+
+testCons1_flattened = [([UnitPow (UnitName "kg") 1.0],[UnitPow (UnitName "m") 1.0])
+                      ,([UnitPow (Determined "x") 1.0],[UnitPow (UnitName "m") 1.0])
+                      ,([UnitPow (Determined "y") 1.0],[UnitPow (UnitName "kg") 1.0])]
+
+testCons1_shifted = [([],[UnitPow (UnitName "m") 1.0,UnitPow (UnitName "kg") (-1.0)])
+                    ,([UnitPow (Determined "x") 1.0],[UnitPow (UnitName "m") 1.0])
+                    ,([UnitPow (Determined "y") 1.0],[UnitPow (UnitName "kg") 1.0])]
+
+--------------------------------------------------
+
+testCons2 = [UnitEq (UnitMul (UnitName "m") (UnitPow (UnitName "s") (-1.0))) (UnitMul (UnitName "m") (UnitPow (UnitName "s") (-1.0)))
+            ,UnitEq (UnitName "m") (UnitMul (UnitMul (UnitName "m") (UnitPow (UnitName "s") (-1.0))) (UnitName "s"))
+            ,UnitEq (UnitAlias "accel") (UnitMul (UnitName "m") (UnitPow (ParametricUse ("simple1_sqr6",0,0)) (-1.0)))
+            ,UnitEq (UnitName "s") (ParametricUse ("simple1_sqr6",1,0))
+            ,UnitEq (Determined "simple1_a5") (UnitAlias "accel")
+            ,UnitEq (Determined "simple1_t4") (UnitName "s")
+            ,UnitEq (Determined "simple1_v3") (UnitMul (UnitName "m") (UnitPow (UnitName "s") (-1.0)))
+            ,UnitEq (Determined "simple1_x1") (UnitName "m")
+            ,UnitEq (Determined "simple1_y2") (UnitName "m")
+            ,UnitEq (ParametricUse ("simple1_sqr6",0,0)) (ParametricUse ("simple1_mul7",0,1))
+            ,UnitEq (ParametricUse ("simple1_sqr6",1,0)) (ParametricUse ("simple1_mul7",1,1))
+            ,UnitEq (ParametricUse ("simple1_sqr6",1,0)) (ParametricUse ("simple1_mul7",2,1))
+            ,UnitEq (ParametricUse ("simple1_mul7",0,1)) (UnitMul (ParametricUse ("simple1_mul7",1,1)) (ParametricUse ("simple1_mul7",2,1)))
+            ,UnitEq (UnitAlias "accel") (UnitMul (UnitName "m") (UnitPow (UnitName "s") (-2.0)))]
+
+testCons2_shifted = [([],[UnitPow (UnitName "m") 1.0,UnitPow (UnitName "s") (-1.0),UnitPow (UnitName "m") (-1.0),UnitPow (UnitName "s") 1.0])
+                    ,([],[UnitPow (UnitName "m") 1.0,UnitPow (UnitName "m") (-1.0)])
+                    ,([UnitPow (UnitAlias "accel") 1.0,UnitPow (ParametricUse ("simple1_sqr6",0,0)) 1.0],[UnitPow (UnitName "m") 1.0])
+                    ,([UnitPow (ParametricUse ("simple1_sqr6",1,0)) (-1.0)],[UnitPow (UnitName "s") (-1.0)])
+                    ,([UnitPow (Determined "simple1_a5") 1.0,UnitPow (UnitAlias "accel") (-1.0)],[])
+                    ,([UnitPow (Determined "simple1_t4") 1.0],[UnitPow (UnitName "s") 1.0])
+                    ,([UnitPow (Determined "simple1_v3") 1.0],[UnitPow (UnitName "m") 1.0,UnitPow (UnitName "s") (-1.0)])
+                    ,([UnitPow (Determined "simple1_x1") 1.0],[UnitPow (UnitName "m") 1.0])
+                    ,([UnitPow (Determined "simple1_y2") 1.0],[UnitPow (UnitName "m") 1.0])
+                    ,([UnitPow (ParametricUse ("simple1_sqr6",0,0)) 1.0,UnitPow (ParametricUse ("simple1_mul7",0,1)) (-1.0)],[])
+                    ,([UnitPow (ParametricUse ("simple1_sqr6",1,0)) 1.0,UnitPow (ParametricUse ("simple1_mul7",1,1)) (-1.0)],[])
+                    ,([UnitPow (ParametricUse ("simple1_sqr6",1,0)) 1.0,UnitPow (ParametricUse ("simple1_mul7",2,1)) (-1.0)],[])
+                    ,([UnitPow (ParametricUse ("simple1_mul7",0,1)) 1.0,UnitPow (ParametricUse ("simple1_mul7",1,1)) (-1.0),UnitPow (ParametricUse ("simple1_mul7",2,1)) (-1.0)],[])
+                    ,([UnitPow (UnitAlias "accel") 1.0],[UnitPow (UnitName "m") 1.0,UnitPow (UnitName "s") (-2.0)])]
