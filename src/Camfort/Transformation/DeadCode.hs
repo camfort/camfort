864250d1f981c193e28582a67a49bf796d2f3b2e
dominic <dom.orchard@gmail.com>
2016-08-15 17:05:53 +0100

    preliminary deadcode elim rewrite

--- src/Camfort/Transformation/DeadCode.hs
+++ src/Camfort/Transformation/DeadCode.hs
@@ -21,36 +21,67 @@
 module Camfort.Transformation.DeadCode where
 
 import Camfort.Analysis.Annotations
-import Camfort.Analysis.LVA
-import Camfort.Analysis.Syntax
-import Camfort.Transformation.Syntax
-import Camfort.Traverse
-import Language.Fortran
-
+import qualified Language.Fortran.Analysis.DataFlow as FAD
+import qualified Language.Fortran.Analysis.Renaming as FAR
+import qualified Language.Fortran.Analysis.BBlocks as FAB
+import qualified Language.Fortran as F
+import qualified Language.Fortran.Util.Position as FU
+import qualified Language.Analysis as FA
 import Camfort.Helpers
 
+import qualified Data.IntMap as IM
+import qualified Data.Set as S
+import Data.Generics.Uniplate.Operations
+import Data.Maybe
 import GHC.Generics
 
 import Debug.Trace
 
-import Data.Generics.Uniplate.Operations
+-- Eliminate dead code from a program, based on the fortran-src
+-- live-variable analysis
 
-deadCode :: Bool -> (Filename, Program Annotation) -> (Report, (Filename, Program Annotation))
-deadCode flag (fname, p) =
-             let (r, p') = mapM ((transformBi elimEmptyFseq) . transformBiM (elimDead flag)) (lva p)
-             in if r == "" then (r, (fname, p'))
-                           else (r, (fname, p')) >>= (deadCode flag)
+-- Currently only strips out dead code through simple variable assignments
+-- but not through array-subscript assignmernts
+deadCode :: Bool -> (Filename, F.ProgramFile A)
+                 -> (Report, (Filename, F.ProgramFile A))
+deadCode flag (fname, pf) = (fname, (report, pf'))
+  where
+    (report, pf'') = deadCode' lva pf'
+    -- initialise analysis
+    pf'   = FAB.analyseBBLocks . FAR.analyseRenames . FA.initAnalysis $ pf
+    -- get map of program unit ==> basic block graph
+    bbm   = FAB.genBBlockMap pf'
+    -- build the supergraph of global dependency
+    sgr   = FAB.genSuperBBGr bbm
+    -- extract the supergraph itself
+    gr    = FAB.superBBGrGraph sgr
+    -- live variables
+    lva   = FAD.liveVariableAnalysis gr
 
-elimEmptyFseq :: Fortran Annotation -> Fortran Annotation
-elimEmptyFseq (FSeq _ _ (NullStmt _ _) n2@(NullStmt _ _)) = n2
-elimEmptyFseq f = f
+deadCode' :: Bool -> InOutMap (S.Set Name)
+                  -> F.ProgramFile (FA.Analysis A)
+                  -> (Report, (F.ProgramFile (FA.Analysis A)))
+deadCode' flag lva pf =
+    if null report
+      then (report, pf')
+      else (report, pf') >>= (deadCode' flag lva)
+  where
+    (report, pf') = mapM (transformBiM (perStmnt flag lva)) pf
 
-elimDead :: Bool -> Fortran Annotation -> (Report, Fortran Annotation)
-elimDead flag x@(Assg a sp@(s1, s2) e1 e2) | (pRefactored a) == flag =
-       let lOut = liveOut a
-          -- currently assumes an assign defines only one access (which is usual)
-       in if ((varExprToAccesses e1) == []) || ((head $ varExprToAccesses e1) `elem` lOut) then 
-              return x
-          else let report = "o" ++ (show . srcLineCol $ s1) ++ ": removed dead code\n"
-               in (report, NullStmt (a { refactored = (Just s1) }) (dropLine sp))
-elimDead _ x = return x
+-- Core of the transformation happens here on assignment statements
+perStmt :: Bool -> InOutMap (S.Set Name) -> F.Statement A -> (Report, F.Statement A)
+perStmt flag lva x@(F.StExpressionAssgign a sp@(FU.SrcSpan s1 s2) e1 e2)
+     | pRefactored a == flag =
+  fromMaybe ("", x) $
+    do id <- FA.insLabel a
+        (_, out) <- IM.lookup lva id
+        assignedName <- varExprToVariableF e1
+        if assignedName `S.elem` out
+          then Just x
+          else -- Dead assignment
+            Just (report, F.StExpressionAssgign a' sp e1 e2)
+              where report =  "o" ++ (show s1) ++ ": removed dead code\n"
+                    -- Set annotation to mark statement for elimination in
+                    -- the reprinter
+                    a' = a { refactored = (Just s1) }) (dropLineF sp)
+perStmt _ _ x = return x
