c178f2d027ccbfd59f095c872766aecbbf18bed8
dominic <dom.orchard@gmail.com>
2017-07-05 21:55:26 +0100

    another example fixed by the previous commit

---
+++ samples/stencils/expected/nested.f90
@@ -0,0 +1,24 @@
+program nested
+  implicit none
+    
+contains
+
+  real function example() 
+    real a(1:10,1:10), b(1:10, 1:10)
+    real :: x, example
+    integer :: i, j
+
+    if (.true.) then
+       do i = 1, 10
+          do j = 1, 10
+             != access readOnce, (pointed(dim=1)) :: a
+             x = max(abs(a(i)), x)
+       end do
+    end do
+    x = x
+    else
+    end if
+    example = a
+  end function example
+
+end program nested
