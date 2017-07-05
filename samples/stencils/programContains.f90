a521e19c22591b58b314149e86fbdc821aa158a4
dominic <dom.orchard@gmail.com>
2017-07-05 21:32:14 +0100

    example for stencils with a program/function

---
+++ samples/stencils/programContains.f90
@@ -0,0 +1,23 @@
+program nested
+  implicit none
+  real a(10)
+  real :: x
+  integer :: i
+
+  do i = 1, 10
+      x = a(i)
+  end do
+  
+  contains
+
+  real function example() 
+    real a(10)
+    real :: x, example
+    integer :: i
+
+    do i = 1, 10
+       x = a(i)
+    end do
+    example = x
+  end function example
+end program nested
