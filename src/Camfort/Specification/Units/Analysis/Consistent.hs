b5f1ca57eafbcb59e79c4c2ff30aa5703d094110
Matthew Danish <mrd45@cam.ac.uk>
2017-08-15 15:56:11 +0100

    Use the Dim interpretation of Constraints to do parametric-equality
    Should fix existing problems with parametric-equality on constraints.

--- src/Camfort/Specification/Units/Analysis/Consistent.hs
+++ src/Camfort/Specification/Units/Analysis/Consistent.hs
@@ -30,6 +30,7 @@ import qualified Camfort.Specification.Units.Annotation as UA
 import           Camfort.Specification.Units.Environment
 import           Camfort.Specification.Units.InferenceBackendSBV (inconsistentConstraints)
 import           Camfort.Specification.Units.Monad
+import qualified Camfort.Specification.Units.BackendTypes as B
 
 import qualified Language.Fortran.AST           as F
 import qualified Language.Fortran.Util.Position as FU
@@ -76,7 +77,8 @@ instance Show ConsistencyError where
 
       findCon :: Constraint -> Maybe FU.SrcSpan
       findCon con = lookupWith (eq con) constraints
-        where eq c1 c2 = or [ conParamEq c1 c2' | c2' <- universeBi c2 ]
+        where -- constraintToDim normalises as it builds the Dim, so we can use dimParamEq directly.
+              eq c1 c2 = or [ B.constraintToDim c1 `B.dimParamEq` B.constraintToDim c2' | c2' <- universeBi c2 ]
       constraints = [ (c, srcSpan)
                     | x <- universeBi pf :: [F.Expression UA]
                     , let srcSpan = FU.getSpan x
