9e8bdb04989517925461494b0bedf5d3104196e8
Bradley Hardy <bradleyhardy@live.com>
2017-08-17 10:47:33 +0100

    Add type signatures in Camfort/Functionality.hs

--- src/Camfort/Functionality.hs
+++ src/Camfort/Functionality.hs
@@ -22,6 +22,8 @@
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
+{-# OPTIONS_GHC -Wall #-}
+
 module Camfort.Functionality (
   -- * Datatypes
     AnnotationType(..)
@@ -53,7 +55,7 @@ import           Camfort.Analysis
   (analysisDebug, analysisInput, analysisResult, branchAnalysis)
 import           Camfort.Analysis.Annotations (mkReport)
 import           Camfort.Analysis.ModFile
-  (genModFiles, readParseSrcDir, simpleCompiler)
+  (genModFiles, readParseSrcDir, simpleCompiler, MFCompiler)
 import           Camfort.Analysis.Simple
 import           Camfort.Input
 import qualified Camfort.Specification.Stencils as Stencils
@@ -68,6 +70,8 @@ import           Camfort.Transformation.CommonBlockElim
 import           Camfort.Transformation.DeadCode
 import           Camfort.Transformation.EquivalenceElim
 
+import           Camfort.Helpers (Filename, FileOrDir)
+
 data AnnotationType = ATDefault | Doxygen | Ford
 
 
@@ -78,25 +82,49 @@ markerChar Doxygen   = '<'
 markerChar Ford      = '!'
 markerChar ATDefault = '='
 
+runFunctionality
+  :: String
+  -> (analysis -> MFCompiler r -> r -> FileOrDir -> FileOrDir -> [Filename] -> IO a)
+  -> analysis
+  -> MFCompiler r
+  -> r
+  -> FileOrDir
+  -> Maybe FileOrDir
+  -> [Filename]
+  -> IO a
 runFunctionality description runner analysis compiler env inSrc incDir excludes = do
   putStrLn $ description ++ " '" ++ inSrc ++ "'"
   incDir' <- maybe getCurrentDirectory pure incDir
   runner analysis compiler env inSrc incDir' excludes
 
+withOutRunner
+  :: (analysis -> MFCompiler r -> r -> FileOrDir -> FileOrDir -> [Filename] -> FilePath -> a)
+  -> FilePath
+  -> analysis
+  -> MFCompiler r
+  -> r
+  -> FileOrDir
+  -> FileOrDir
+  -> [Filename]
+  -> a
 withOutRunner runner outSrc analysis compiler env inSrc incDir excludes
   = runner analysis compiler env inSrc incDir excludes outSrc
 
 -- * Wrappers on all of the features
+ast :: FileOrDir -> Maybe FilePath -> [Filename] -> IO ()
 ast d incDir excludes = do
     incDir' <- maybe getCurrentDirectory pure incDir
     modFiles <- genModFiles simpleCompiler () incDir' excludes
     xs <- readParseSrcDir modFiles d excludes
     print . fmap fst $ xs
 
+countVarDecls :: String -> Maybe FilePath -> [Filename] -> IO ()
 countVarDecls =
   runFunctionality "Counting variable declarations in"
     doAnalysisReport countVariableDeclarations simpleCompiler ()
 
+dead ::
+  String -> Maybe FilePath -> [Filename] -> FileOrDir -> IO ()
 dead inSrc incDir excludes outSrc =
   let rfun = do
         pfs <- analysisInput
@@ -106,11 +134,15 @@ dead inSrc incDir excludes outSrc =
   in runFunctionality "Eliminating dead code in" (withOutRunner doRefactor outSrc)
      rfun simpleCompiler () inSrc incDir excludes
 
+common
+  :: String -> Maybe FilePath -> [Filename] -> FileOrDir -> IO ()
 common inSrc incDir excludes outSrc =
   let rfun = commonElimToModules (takeDirectory outSrc ++ "/")
   in runFunctionality "Refactoring common blocks in"
      (withOutRunner doRefactorAndCreate outSrc) rfun simpleCompiler () inSrc incDir excludes
 
+equivalences
+  :: String -> Maybe FilePath -> [Filename] -> FileOrDir -> IO ()
 equivalences inSrc incDir excludes outSrc = do
   let rfun = do
         pfs <- analysisInput
@@ -122,6 +154,22 @@ equivalences inSrc incDir excludes outSrc = do
 
 {- Units feature -}
 
+runUnitsFunctionality
+  :: String
+     -> (analysis
+         -> MFCompiler UnitOpts
+         -> UnitOpts
+         -> FileOrDir
+         -> FileOrDir
+         -> [Filename]
+         -> IO a)
+     -> analysis
+     -> FileOrDir
+     -> Maybe FileOrDir
+     -> [Filename]
+     -> LiteralsOpt
+     -> Bool
+     -> IO a
 runUnitsFunctionality description runner analysis inSrc incDir excludes m debug =
   let uo = optsToUnitOpts m debug
   in runFunctionality description runner analysis compileUnits uo inSrc incDir excludes
@@ -132,11 +180,23 @@ optsToUnitOpts m debug = o1
                        , uoDebug = debug
                        }
 
+unitsCheck ::
+  FileOrDir
+  -> Maybe FileOrDir -> [Filename] -> LiteralsOpt -> Bool -> IO ()
 unitsCheck = runUnitsFunctionality "Checking units for" doAnalysisReport checkUnits
 
+unitsInfer ::
+  FileOrDir
+  -> Maybe FileOrDir -> [Filename] -> LiteralsOpt -> Bool -> IO ()
 unitsInfer = runUnitsFunctionality "Inferring units for" doAnalysisReport inferUnits'
   where inferUnits' = (mkReport . either show show) <$> inferUnits
 
+unitsSynth
+  :: String -> Maybe FilePath -> [Filename]
+  -> LiteralsOpt -> Bool
+  -> FileOrDir
+  -> AnnotationType
+  -> IO ()
 unitsSynth inSrc incDir excludes m debug outSrc annType =
   let marker = markerChar annType
       rfun = do
@@ -151,22 +211,49 @@ unitsSynth inSrc incDir excludes m debug outSrc annType =
       runner' = withOutRunner doRefactor outSrc
   in runUnitsFunctionality "Synthesising units for" runner' rfun inSrc incDir excludes m debug
 
+unitsCriticals ::
+  FileOrDir
+  -> Maybe FileOrDir -> [Filename] -> LiteralsOpt -> Bool -> IO ()
 unitsCriticals =
   runUnitsFunctionality "Suggesting variables to annotate with unit specifications in"
     doAnalysisReport inferCriticalVariables
 
 {- Stencils feature -}
 
+runStencilsFunctionality
+  :: String
+     -> (analysis
+         -> MFCompiler ()
+         -> ()
+         -> FileOrDir
+         -> FileOrDir
+         -> [Filename]
+         -> IO a)
+     -> analysis
+     -> FileOrDir
+     -> Maybe FileOrDir
+     -> [Filename]
+     -> IO a
 runStencilsFunctionality description runner analysis =
   runFunctionality description runner analysis compileStencils ()
 
+stencilsCheck :: String -> Maybe FilePath -> [Filename] -> IO ()
 stencilsCheck =
   runStencilsFunctionality "Checking stencil specs for" doAnalysisReport Stencils.check
 
+stencilsInfer
+  :: String -> Maybe FilePath -> [Filename] -> Bool -> IO ()
 stencilsInfer inSrc incDir excludes useEval =
   let rfun = Stencils.infer useEval '='
   in runStencilsFunctionality "Inferring stencil specs for" doAnalysisReport rfun inSrc incDir excludes
 
+stencilsSynth
+  :: String
+  -> Maybe FilePath
+  -> [Filename]
+  -> AnnotationType
+  -> FileOrDir
+  -> IO ()
 stencilsSynth inSrc incDir excludes annType outSrc =
   let rfun = second (fmap (pure :: a -> Either () a)) <$> Stencils.synth (markerChar annType)
       runner' = withOutRunner doRefactor outSrc
