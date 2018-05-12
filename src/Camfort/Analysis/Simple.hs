af4031ee08536dffed3d00deefb6edc73340a19f
Matthew Danish <mrd45@cam.ac.uk>
2018-05-12 16:41:23 +0100

    change reporting style of implicit-none to use logError

--- src/Camfort/Analysis/Simple.hs
+++ src/Camfort/Analysis/Simple.hs
@@ -22,6 +22,7 @@
 module Camfort.Analysis.Simple
  (countVariableDeclarations, checkImplicitNone, ImplicitNoneReport(..)) where
 import Prelude hiding (unlines)
+import Control.Monad
 import Data.Data
 import Data.Text (Text, unlines)
 import Data.Monoid ((<>))
@@ -33,27 +34,39 @@ import qualified Data.Text.Lazy.Builder as Builder
 import qualified Language.Fortran.AST as F
 import qualified Language.Fortran.Util.Position as F
 
-import Camfort.Analysis (describe, describeBuilder, PureAnalysis, Describe)
+import Camfort.Analysis ( ExitCodeOfReport(..), atSpanned, Origin, failAnalysis'
+                        , logError', logError, describe, describeBuilder
+                        , PureAnalysis, Describe )
 
 {-| Counts the number of declarations (of variables) in a whole program -}
 
 countVariableDeclarations :: forall a. Data a => F.ProgramFile a -> PureAnalysis () () Int
 countVariableDeclarations pf = return $ length (universeBi pf :: [F.Declarator a])
 
+type PULoc = (F.ProgramUnitName, Origin)
 data ImplicitNoneReport
-  = ImplicitNoneReport [(F.ProgramUnitName, F.SrcSpan)] -- ^ list of program units identified as needing implicit none
+  = ImplicitNoneReport [PULoc] -- ^ list of program units identified as needing implicit none
+
+instance Monoid ImplicitNoneReport where
+  mempty = ImplicitNoneReport []
+  ImplicitNoneReport r1 `mappend` ImplicitNoneReport r2 = ImplicitNoneReport $ r1 ++ r2
 
 -- Follows host scoping unit rule: 'external' program units
 -- (ie. appear at top level) must have implicit-none statements, which
 -- are then inherited by internal subprograms. Also, subprograms of
 -- interface blocks must have implicit-none statements. FIXME: when we
 -- have Fortran 2008 support then submodules must also be checked.
-checkImplicitNone :: forall a. Data a => F.ProgramFile a -> PureAnalysis () () ImplicitNoneReport
+checkImplicitNone :: forall a. Data a => F.ProgramFile a -> PureAnalysis String () ImplicitNoneReport
 checkImplicitNone pf = do
-  let checkedPUs = [ puHelper pu | pu <- childrenBi pf :: [F.ProgramUnit a] ]
-  let checkedInterfaces = [ puHelper pu | int@(F.BlInterface {}) <- universeBi pf :: [F.Block a]
-                                        , pu <- childrenBi int :: [F.ProgramUnit a] ]
-  return . ImplicitNoneReport . concat $ checkedPUs ++ checkedInterfaces
+  checkedPUs <- sequence [ puHelper pu | pu <- childrenBi pf :: [F.ProgramUnit a] ]
+  checkedInterfaces <- sequence [ puHelper pu | int@(F.BlInterface {}) <- universeBi pf :: [F.Block a]
+                                              , pu <- childrenBi int :: [F.ProgramUnit a] ]
+  forM_ (checkedPUs ++ checkedInterfaces) $ \ r -> case r of
+     ImplicitNoneReport [(F.Named name, orig)] -> logError orig name
+     _ -> return ()
+
+  return $ mconcat checkedPUs <> mconcat checkedInterfaces
+
   where
     isUseStmt (F.BlStatement _ _ _ (F.StUse {})) = True; isUseStmt _ = False
     isImplicitNone (F.BlStatement _ _ _ (F.StImplicit _ _ Nothing)) = True; isImplicitNone _ = False
@@ -71,10 +84,13 @@ checkImplicitNone pf = do
       where
         (useStmts, rest) = break isImplicitNone bs
 
-    puHelper pu = if checkPU pu then [] else [(F.getName pu, F.getSpan pu)]
+    puHelper pu
+      | checkPU pu = return mempty
+      | otherwise = fmap (\ o -> ImplicitNoneReport [(F.getName pu, o)]) $ atSpanned pu
 
 
 instance Describe ImplicitNoneReport where
-  describeBuilder (ImplicitNoneReport results) = Builder.fromText . unlines . map descResult $ results
-    where
-      descResult (F.Named puName, srcSpan) = describe srcSpan <> " " <> describe puName
+  describeBuilder (ImplicitNoneReport results)
+    | null results = "no cases detected"
+    | null (tail results) = "1 case detected"
+    | otherwise = Builder.fromText $ describe (length results) <> " cases detected"
