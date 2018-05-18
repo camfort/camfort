0962dee21ccda8499a67e981b18f2b327ef77a06
Dominic Orchard <dom.orchard@gmail.com>
2018-05-18 17:28:55 +0100

    rads/degrees example

---
+++ samples/invariants/angle.f90
@@ -0,0 +1,22 @@
+! Verify with CamFort using:
+!  camfort invariants-check angle.f90
+
+program degrees
+  implicit none
+
+  real, parameter :: deg = -45
+  real rad
+
+  rad = toRad(deg)
+  print *, rad
+  
+contains
+
+  != static_assert pre("deg >= 0" & "deg <= 360")
+  != static_assert post("toRad >= 0" & "toRad <= 6.284")
+  real function toRad(deg)
+    real deg
+    real, parameter :: pi = 3.14159265358979323864
+    toRad = 2 * pi * (deg/360)
+  end function toRad
+end program degrees
