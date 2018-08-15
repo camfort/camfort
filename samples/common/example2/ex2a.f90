b91a42413552f7a2e97c439b616a61d24c524b36
Andrew Rice <acr31@cam.ac.uk>
2018-08-15 16:13:05 +0100

    removed compilation comments since they are now in the makefile

--- samples/common/example2/ex2a.f90
+++ samples/common/example2/ex2a.f90
@@ -1,9 +1,3 @@
-! camfort common ex2a.f90 out2a.f90
-! camfort common ex2b.f90 out2b.f90
-
-! gfortran -c shared.f90
-! gfortran -o ex1 out1a.f90 out1b.f90 shared.f90
-
 program ex2a
   real x(2:10), y
   common /shared/ x, y
