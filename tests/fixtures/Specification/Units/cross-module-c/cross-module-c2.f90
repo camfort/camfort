ed99af4a7232bc316e53d454007070ac099bdd16
Dominic Orchard <dom.orchard@gmail.com>
2024-09-02 17:22:09 +0100

    mwe for #185

---
+++ tests/fixtures/Specification/Units/cross-module-c/cross-module-c2.f90
@@ -0,0 +1,6 @@
+program c2
+  implicit none
+  use c1
+  real :: t
+  a = b / t
+end program c2
---
+++ tests/fixtures/Specification/Units/cross-module-c/cross-module-c3.f90
@@ -0,0 +1,9 @@
+module c3
+  implicit none
+  ! some constant
+  real :: a3 = 1.0
+  ! another constant
+  !
+  ! another constant
+  real :: b3 = 2.0
+end module c3
