78dd02ed980c28fe2e7df633980ac43ed8016722
Ben Orchard <thefirstmuffinman@gmail.com>
2024-09-12 16:27:10 +0100

    try to bump GHC version, Nix flakes
    WIP. Nix won't build and deps probably won't resolve.
    fortran-src uses singletons-th which is very picky.

--- flake.nix
+++ flake.nix
@@ -10,55 +10,34 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
     haskell-flake.url = "github:srid/haskell-flake";
+    fortran-src.url   = "github:camfort/fortran-src";
+    fortran-src.flake = false;
   };
   outputs = inputs@{ self, nixpkgs, flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = nixpkgs.lib.systems.flakeExposed;
       imports = [ inputs.haskell-flake.flakeModule ];
       perSystem = { self', pkgs, config, ... }: {
-        packages.default = self'.packages.camfort;
-        #haskellProjects.ghc96 = import ./haskell-flake-ghc96.nix pkgs;
-        haskellProjects.default = {
-          #basePackages = config.haskellProjects.ghc96.outputs.finalPackages;
-          packages = {
-            fortran-src.source = "0.11.0";
-          };
-
+        packages.default = self'.packages.ghc96-camfort;
+        devShells.default = self'.devShells.ghc96;
+        haskellProjects.ghc96 = {
+          basePackages = pkgs.haskell.packages.ghc96;
+          packages.fortran-src.source = inputs.fortran-src;
           settings = {
             sbv = {
-              # 2023-04-18 raehik: sbv-9.0 broken; seems tests fail. try ignoring
-              check = false;
               broken = false;
-            };
-          };
-
-          devShell = {
-            # libflint doesn't get configured properly for `cabal repl` -- fix
-            # it here
-            # curiously, `cabal build` works... maybe $NIX_LDFLAGS etc. get used
-            # by the GHC invocation involved there.
-            # I don't have to do this for pkg-config libraries (of which
-            # libflint is not one). interesting!
-            mkShellArgs.shellHook = ''
-              export LD_LIBRARY_PATH='${pkgs.flint}/lib'
-            '';
-
-            tools = hp: {
-              ghcid = null; # broken on GHC 9.6? old fsnotify
-              hlint = null; # broken on GHC 9.6? old
-              haskell-language-server = null; # TAKES AGES TO BUILD FFS
             };
           };
         };
 
         # use streamLayeredImage so as to not place the image in the Nix store
-        packages.image = pkgs.dockerTools.streamLayeredImage {
+        packages.ghc96-camfort-image = pkgs.dockerTools.streamLayeredImage {
           name = "camfort";
           # equivalent to `git rev-parse HEAD`
           # only exists on clean working tree, else set to "dev"
           tag = self.rev or "dev";
           config = {
-            Entrypoint = [ "${pkgs.lib.getExe self'.packages.camfort}" ];
+            Entrypoint = [ "${pkgs.lib.getExe self'.packages.ghc96-camfort}" ];
           };
           maxLayers = 120; # less than Docker max layers to allow extending
           # Note that Z3 is configured using the C bindings, which then access
