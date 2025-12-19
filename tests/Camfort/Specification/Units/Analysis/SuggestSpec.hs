2bc7d3595679e0e405a640be931306227cba333c
Dominic Orchard <dom.orchard@gmail.com>
2025-12-19 11:00:42 +0000

    add suggest spec stuff

---
+++ tests/Camfort/Specification/Units/Analysis/SuggestSpec.hs
@@ -0,0 +1,49 @@
+module Camfort.Specification.Units.Analysis.SuggestSpec (spec) where
+
+import           Camfort.Analysis hiding (describe)
+import           Camfort.Analysis.ModFile (readParseSrcDir, genModFiles)
+import           Camfort.Specification.Units.Analysis (compileUnits)
+import           Camfort.Specification.Units.Analysis.Criticals (inferCriticalVariables)
+import           Camfort.Specification.Units.Monad (LiteralsOpt(..), unitOpts0, UnitOpts, uoLiterals, uninitializeds, UnitEnv(..), runUnitAnalysis)
+import           Control.Lens
+import           Language.Fortran.Util.ModFile (emptyModFiles)
+import           System.FilePath ((</>))
+import qualified Test.Hspec as Test
+import           Test.Hspec hiding (Spec)
+import           Camfort.TestUtils (hideFormatting, normalisedShouldBe)
+import           Language.Fortran.Util.ModFile
+
+spec :: Test.Spec
+spec = do
+  describe "units-suggest (poly literals mode)" $ do
+    it "suggests get_lightspeed for internals.f90" $
+       unitsSuggestReportIs LitPoly False [] "internals.f90" expectedInternalsPolyReport
+
+fixturesDir :: String
+fixturesDir = "tests" </> "fixtures" </> "Specification" </> "Units"
+
+-- | Assert that the report of performing units suggest on a file is as expected.
+unitsSuggestReportIs :: LiteralsOpt -> Bool -> [String] -> String -> String -> Expectation
+unitsSuggestReportIs litmode uninitmode modNames fileName expectedReport = do
+  let file = fixturesDir </> fileName
+      modPaths = fmap (fixturesDir </>) modNames
+  modFiles <- mapM (mkTestModFile uOpts) modPaths
+  [(pf,_)] <- readParseSrcDir Nothing modFiles file []
+
+  let uEnv = UnitEnv { unitOpts = uOpts, unitProgramFile = pf }
+
+  report <- runAnalysisT file (logOutputNone True) LogError modFiles $ runUnitAnalysis uEnv $ inferCriticalVariables ""
+  let res = report ^?! arResult . _ARSuccess
+
+  hideFormatting (show res) `normalisedShouldBe` expectedReport
+  where uOpts = unitOpts0 { uoLiterals = litmode, uninitializeds = uninitmode }
+
+-- | Helper for producing a basic ModFile from a (terminal) module file.
+mkTestModFile :: UnitOpts -> String -> IO ModFile
+mkTestModFile uopts file =
+  head <$> genModFiles Nothing emptyModFiles compileUnits uopts file []
+
+expectedInternalsPolyReport :: String
+expectedInternalsPolyReport =
+  "\ntests" </> "fixtures" </> "Specification" </> "Units" </> "internals.f90: 1 variable declarations suggested to be given a specification:\n\
+  \    tests" </> "fixtures" </> "Specification" </> "Units" </> "internals.f90:9:19    get_lightspeed\n"
