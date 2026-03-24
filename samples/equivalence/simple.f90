3bf89f0356baf1ed8f9ceae559e5fa59153942d1
Nikolaus Huber <mail@n-huber.eu>
2026-03-24 13:57:26 +0000

    Extend equivalence example

--- samples/equivalence/simple.f90
+++ samples/equivalence/simple.f90
@@ -1,7 +1,19 @@
 program example
     integer(kind=4) :: a, b
-    character :: x(4)
+    integer(kind=8) :: c
+    real(kind=8) :: r1
+    real(kind=16) :: r2
+    character :: x(4), y(4)
 
-    equivalence (a, b)      ! endian-portable
-    equivalence (a, x)      ! not endian-portable
+    equivalence (a, b)        ! safe
+    equivalence (c, r1)       ! safe
+    equivalence (x, y)        ! safe
+    equivalence (a, c)        ! not safe
+    equivalence (a, x(1))     ! not safe
+    equivalence (a, x)        ! not safe
+    equivalence (r1, r2)      ! not safe
 end program example
+
+! "use-site" analysis
+! views, permutations, etc.
+! maybe the way expressions are used mitigate
